8d4bc308074bf76aa2eb2bf4eab0039e50abcbb3
Charlie Haag <charlottehaag22@gmail.com>
2019-04-10 14:48:47 -0700

    set up for deploy to production

--- server/app.js
+++ server/app.js
@@ -5,7 +5,11 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var cors = require('cors');
 
+const appConfig = require('./config/main-config.js');
 const routeConfig = require('./config/route-config.js');
+
+appConfig.init();
+routeConfig.init(app);
 
 var app = express();
 
@@ -19,8 +23,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-routeConfig.init(app);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
